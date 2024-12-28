54943758943c8746ea279bd23027fe2843e83cb5
Alex <alexgatty0608@gmail.com>
2024-12-28 16:05:30 +0000

    I have added all methods besides brackets.  Wwhen printing values it does each one on a new line so will need to change it so they are inline.

---
+++ interpreter.py
@@ -0,0 +1,32 @@
+code=input("please enter the brainf*ck code\n")
+values=[0]
+pointer=0
+
+for x in code:
+    if x=="+":
+        values[pointer]=values[pointer]+1
+        if values[pointer]==256:
+            values[pointer]=0
+        
+    elif x=="-":
+        values[pointer]=values[pointer]-1
+        if values[pointer]==-1:
+            values[pointer]=255
+    
+    elif x=="<":
+        pointer=pointer-1
+    elif x==">":
+        pointer=pointer+1
+        if pointer==len(values):
+            values.append(0)
+    
+    elif x==".":
+        print(chr(values[pointer]))
+
+    elif x==",":
+        user=input("character:")
+        values[pointer]=ord(user)
+
+
+print(values)
+    
