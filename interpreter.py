9c8c27d561135a37cc4d156c8979ba876fc0f43f
Alex <alexgatty0608@gmail.com>
2024-12-29 12:52:43 +0000

    I have added loops so that having brackets works.  I need to change it to funcitons to make the code more efficient but also allow me to have brackets in brackets because the code currently cant handle it.

--- interpreter.py
+++ interpreter.py
@@ -1,7 +1,8 @@
 code=input("please enter the brainf*ck code\n")
 values=[0]
 pointer=0
-
+stack=[]
+counter=0
 for x in code:
     if x==",":
         user=input("character:")
@@ -27,6 +28,40 @@ for x in code:
     elif x==".":
         print(chr(values[pointer]),end="")
 
+    elif x=="[":
+        stack.append(counter)
+
+    elif x=="]":
+        start=stack.pop()
+        end=counter
+        while values[pointer]!=0:
+           new=code[start+1:counter]
+           for x in new:
+            if x==",":
+                user=input("character:")
+                values[pointer]=ord(user)
     
-print(values)
+            elif x=="+":
+                values[pointer]=values[pointer]+1
+            if values[pointer]==256:
+                values[pointer]=0
+        
+            elif x=="-":
+                values[pointer]=values[pointer]-1
+            if values[pointer]==-1:
+                values[pointer]=255
     
+            elif x=="<":
+                pointer=pointer-1
+            elif x==">":
+                pointer=pointer+1
+            if pointer==len(values):
+                values.append(0)
+    
+            elif x==".":
+                print(chr(values[pointer]),end="")
+           
+
+    counter=counter+1
+
+
